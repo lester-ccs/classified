584716dc2f8dd96bc00aed3bc17defcfe356dc4f
Lester Hurtado <lester@hashtagdigital.com>
2017-06-26 12:34:09 +0000

    Modify (break up) the default template.

---
+++ resources/views/layouts/partials/_head.blade.php
@@ -0,0 +1,18 @@
+{{--
+  -- Created by PhpStorm.
+  -- User: Lester Hurtado
+  -- Date: 6/26/17
+  -- Time: 8:04 PM
+--}}
+
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!-- CSRF Token -->
+<meta name="csrf-token" content="{{ csrf_token() }}">
+
+<title>{{ config('app.name', 'Laravel') }}</title>
+
+<!-- Styles -->
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
