77d2acff724dc20f8b30e98cd55faac9c5071f3a
Lester Hurtado <lester@hashtagdigital.com>
2017-06-26 14:17:18 +0000

    Modify routes in web.php, generated areas in HomeController and pass it to home.blade.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,26 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Area;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        return view('home');
+        $areas = Area::get()->toTree();
+
+        return view('home', compact('areas'));
     }
 }
